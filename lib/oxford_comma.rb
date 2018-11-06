cde818d5abcb03e96397a7044ef8ca5636846815
Nathan Bell <nathanbell09@gmail.com>
2018-11-06 21:44:53 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,11 @@
 def oxford_comma(array)
-  if array.size == 1
-    array.join
+  case array.size
+    when 1
+      array.join
+    when 2
+      array.join(" and ")
+    else
+      last_element = array.pop
+      array.join(", ") + ", and #{last_element}"
   end
 end
-
-tester = ["test"]
-puts oxford_comma(tester)
